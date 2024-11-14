d882c5bcaa39f3875fecee9a991d81f01f634577
OnvaraChaichoopol <onvara.cha@dome.tu.ac.th>
2024-11-15 01:18:49 +0700

    first commit

---
+++ FireDB/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace FireDB;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+		MainPage = new StudentPage();
+
+	}
+}
